e208abc968b3d378fbe07cc316e7fb8077ad9ef2
Andrii0302 <144677052+Andrii0302@users.noreply.github.com>
2023-11-22 13:11:49 +0200

    Update views.py

--- user/views.py
+++ user/views.py
@@ -105,15 +105,22 @@ class Login(APIView):
 
 class UserView(APIView):
     def get(self, request):
-        token = request.COOKIES.get('jwt')
+       
+        token = request.data.get('jwt')
+
         if not token:
             raise AuthenticationFailed("Unauthenticated")
+
         try:
-            
             payload = jwt.decode(token, 'secret', algorithms=['HS256'])
         except jwt.ExpiredSignatureError:
             raise AuthenticationFailed("Unauthenticated")
+
         user = User.objects.filter(id=payload['id']).first()
+        
+        if not user:
+            raise AuthenticationFailed("User not found")
+
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
